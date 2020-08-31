f39347b5a3484594bbf030afb99edc33ff6e3530
SYangChen <yang870313@gmail.com>
2020-08-31 15:23:40 +0800

    2nd edition architecture by change 1 input to 50 at once

--- FPGA_model-architecture/memory.v
+++ FPGA_model-architecture/memory.v
@@ -1,10 +1,10 @@
-module memory( reset, en, r_w, abus, dbus_in, dbus_out ) ;
-	input reset, en, r_w ;
-	input [7:0] abus ;
-	input [7:0] dbus_in ;
-	output reg [7:0] dbus_out ;
+module memory50( reset, en, r_w, abus, insign, indata, dbus_in1, dbus_in2, dbus_in3, dbus_in4, dbus_in5, dbus_in6, dbus_in7, dbus_in8, dbus_in9, dbus_in10, dbus_in11, dbus_in12, dbus_in13, dbus_in14, dbus_in15, dbus_in16, dbus_in17, dbus_in18, dbus_in19,  dbus_in20, dbus_in21, dbus_in22, dbus_in23, dbus_in24, dbus_in25, dbus_in26, dbus_in27, dbus_in28, dbus_in29, dbus_in30, dbus_in31, dbus_in32, dbus_in33, dbus_in34, dbus_in35, dbus_in36, dbus_in37, dbus_in38, dbus_in39, dbus_in40, dbus_in41, dbus_in42, dbus_in43, dbus_in44, dbus_in45, dbus_in46, dbus_in47, dbus_in48, dbus_in49, dbus_in50, dbus_out1, dbus_out2, dbus_out3, dbus_out4, dbus_out5, dbus_out6, dbus_out7, dbus_out8, dbus_out9, dbus_out10, dbus_out11, dbus_out12, dbus_out13, dbus_out14, dbus_out15, dbus_out16, dbus_out17, dbus_out18, dbus_out19,  dbus_out20, dbus_out21, dbus_out22, dbus_out23, dbus_out24, dbus_out25, dbus_out26, dbus_out27, dbus_out28, dbus_out29, dbus_out30, dbus_out31, dbus_out32, dbus_out33, dbus_out34, dbus_out35, dbus_out36, dbus_out37, dbus_out38, dbus_out39, dbus_out40, dbus_out41, dbus_out42, dbus_out43, dbus_out44, dbus_out45, dbus_out46, dbus_out47, dbus_out48, dbus_out49, dbus_out50 ) ;
+	input reset, en, r_w, insign ;
+	input [15:0] abus ;
+	input [7:0] indata, dbus_in1, dbus_in2, dbus_in3, dbus_in4, dbus_in5, dbus_in6, dbus_in7, dbus_in8, dbus_in9, dbus_in10, dbus_in11, dbus_in12, dbus_in13, dbus_in14, dbus_in15, dbus_in16, dbus_in17, dbus_in18, dbus_in19,  dbus_in20, dbus_in21, dbus_in22, dbus_in23, dbus_in24, dbus_in25, dbus_in26, dbus_in27, dbus_in28, dbus_in29, dbus_in30, dbus_in31, dbus_in32, dbus_in33, dbus_in34, dbus_in35, dbus_in36, dbus_in37, dbus_in38, dbus_in39, dbus_in40, dbus_in41, dbus_in42, dbus_in43, dbus_in44, dbus_in45, dbus_in46, dbus_in47, dbus_in48, dbus_in49, dbus_in50 ;
+	output reg [7:0] dbus_out1, dbus_out2, dbus_out3, dbus_out4, dbus_out5, dbus_out6, dbus_out7, dbus_out8, dbus_out9, dbus_out10, dbus_out11, dbus_out12, dbus_out13, dbus_out14, dbus_out15, dbus_out16, dbus_out17, dbus_out18, dbus_out19,  dbus_out20, dbus_out21, dbus_out22, dbus_out23, dbus_out24, dbus_out25, dbus_out26, dbus_out27, dbus_out28, dbus_out29, dbus_out30, dbus_out31, dbus_out32, dbus_out33, dbus_out34, dbus_out35, dbus_out36, dbus_out37, dbus_out38, dbus_out39, dbus_out40, dbus_out41, dbus_out42, dbus_out43, dbus_out44, dbus_out45, dbus_out46, dbus_out47, dbus_out48, dbus_out49, dbus_out50 ;
 	
-	reg [7:0] m [0:128] ;
+	reg [7:0] m [0:2047] ;
 	reg [7:0] data ;
 	reg [7:0] i ;
 
@@ -12,34 +12,178 @@ module memory( reset, en, r_w, abus, dbus_in, dbus_out ) ;
     begin
         if ( reset == 1 )
         begin
-            m[0] <= 8'h20 ;
-            m[1] <= 8'h10 ;
-            m[2] <= 8'h20 ;
-            m[3] <= 8'h10 ;
-            m[4] <= 8'h10 ;
-			m[5] <= 8'h10 ;
-			m[6] <= 8'h10 ;
-			m[7] <= 8'h10 ;
-			m[8] <= 8'h10 ;
-			m[9] <= 8'h10 ;
-			m[10] <= 8'h10 ;
-			dbus_out = 8'h00 ;
+            m[1000] = 8'h10 ;
+			m[1101] = 8'h10 ;
+			dbus_out1 = 8'h00 ;
         end
         else if ( en == 1 && r_w == 0 ) // r_w==0:write
         begin
             // data = dbus_in ;
-            m[abus] = dbus_in ;
-			dbus_out = dbus_in ;
+			if ( insign ) begin
+				m[abus] = indata ;
+				dbus_out1 = indata ;
+			end
+			else begin
+				m[abus] = dbus_in1 ;
+				m[abus+1] = dbus_in2 ;
+				m[abus+2] = dbus_in3 ;
+				m[abus+3] = dbus_in4 ;
+				m[abus+4] = dbus_in5 ;
+				m[abus+5] = dbus_in6 ;
+				m[abus+6] = dbus_in7 ;
+				m[abus+7] = dbus_in8 ;
+				m[abus+8] = dbus_in9 ;
+				m[abus+9] = dbus_in10 ;
+				m[abus+10] = dbus_in11 ;
+				m[abus+11] = dbus_in12 ;
+				m[abus+12] = dbus_in13 ;
+				m[abus+13] = dbus_in14 ;
+				m[abus+14] = dbus_in15 ;
+				m[abus+15] = dbus_in16 ;
+				m[abus+16] = dbus_in17 ;
+				m[abus+17] = dbus_in18 ;
+				m[abus+18] = dbus_in19 ;
+				m[abus+19] = dbus_in20 ;
+				m[abus+20] = dbus_in21 ;
+				m[abus+21] = dbus_in22 ;
+				m[abus+22] = dbus_in23 ;
+				m[abus+23] = dbus_in24 ;
+				m[abus+24] = dbus_in25 ;
+				m[abus+25] = dbus_in26 ;
+				m[abus+26] = dbus_in27 ;
+				m[abus+27] = dbus_in28 ;
+				m[abus+28] = dbus_in29 ;
+				m[abus+29] = dbus_in30 ;
+				m[abus+30] = dbus_in31 ;
+				m[abus+31] = dbus_in32 ;
+				m[abus+32] = dbus_in33 ;
+				m[abus+33] = dbus_in34 ;
+				m[abus+34] = dbus_in35 ;
+				m[abus+35] = dbus_in36 ;
+				m[abus+36] = dbus_in37 ;
+				m[abus+37] = dbus_in38 ;
+				m[abus+38] = dbus_in39 ;
+				m[abus+39] = dbus_in40 ;
+				m[abus+40] = dbus_in41 ;
+				m[abus+41] = dbus_in42 ;
+				m[abus+42] = dbus_in43 ;
+				m[abus+43] = dbus_in44 ;
+				m[abus+44] = dbus_in45 ;
+				m[abus+45] = dbus_in46 ;
+				m[abus+46] = dbus_in47 ;
+				m[abus+47] = dbus_in48 ;
+				m[abus+48] = dbus_in49 ;
+				m[abus+49] = dbus_in50 ;
+				dbus_out1 = dbus_in1 ;
+			end
         end
         else if ( en == 1 && r_w == 1 ) // r_w==1:read
 		begin
             // data = m[abus] ;
-			dbus_out = m[abus] ;
+			dbus_out1 = m[abus] ;
+			dbus_out2 = m[abus+1] ;
+			dbus_out3 = m[abus+2] ;
+			dbus_out4 = m[abus+3] ;
+			dbus_out5 = m[abus+4] ;
+			dbus_out6 = m[abus+5] ;
+			dbus_out7 = m[abus+6] ;
+			dbus_out8 = m[abus+7] ;
+			dbus_out9 = m[abus+8] ;
+			dbus_out10 = m[abus+9] ;
+			dbus_out11 = m[abus+10] ;
+			dbus_out12 = m[abus+11] ;
+			dbus_out13 = m[abus+12] ;
+			dbus_out14 = m[abus+13] ;
+			dbus_out15 = m[abus+14] ;
+			dbus_out16 = m[abus+15] ;
+			dbus_out17 = m[abus+16] ;
+			dbus_out18 = m[abus+17] ;
+			dbus_out19 = m[abus+18] ;
+			dbus_out20 = m[abus+19] ;
+			dbus_out21 = m[abus+20] ;
+			dbus_out22 = m[abus+21] ;
+			dbus_out23 = m[abus+22] ;
+			dbus_out24 = m[abus+23] ;
+			dbus_out25 = m[abus+24] ;
+			dbus_out26 = m[abus+25] ;
+			dbus_out27 = m[abus+26] ;
+			dbus_out28 = m[abus+27] ;
+			dbus_out29 = m[abus+28] ;
+			dbus_out30 = m[abus+29] ;
+			dbus_out31 = m[abus+30] ;
+			dbus_out32 = m[abus+31] ;
+			dbus_out33 = m[abus+32] ;
+			dbus_out34 = m[abus+33] ;
+			dbus_out35 = m[abus+34] ;
+			dbus_out36 = m[abus+35] ;
+			dbus_out37 = m[abus+36] ;
+			dbus_out38 = m[abus+37] ;
+			dbus_out39 = m[abus+38] ;
+			dbus_out40 = m[abus+39] ;
+			dbus_out41 = m[abus+40] ;
+			dbus_out42 = m[abus+41] ;
+			dbus_out43 = m[abus+42] ;
+			dbus_out44 = m[abus+43] ;
+			dbus_out45 = m[abus+44] ;
+			dbus_out46 = m[abus+45] ;
+			dbus_out47 = m[abus+46] ;
+			dbus_out48 = m[abus+47] ;
+			dbus_out49 = m[abus+48] ;
+			dbus_out50 = m[abus+49] ;
 		end
         else
 		begin
             // data = 8'hZZ ;
-			dbus_out = 8'hzz ;
+			dbus_out1 = 8'hzz ;
+			dbus_out2 = 8'hzz ;
+			dbus_out3 = 8'hzz ;
+			dbus_out4 = 8'hzz ;
+			dbus_out5 = 8'hzz ;
+			dbus_out6 = 8'hzz ;
+			dbus_out7 = 8'hzz ;
+			dbus_out8 = 8'hzz ;
+			dbus_out9 = 8'hzz ;
+			dbus_out10 = 8'hzz ;
+			dbus_out11 = 8'hzz ;
+			dbus_out12 = 8'hzz ;
+			dbus_out13 = 8'hzz ;
+			dbus_out14 = 8'hzz ;
+			dbus_out15 = 8'hzz ;
+			dbus_out16 = 8'hzz ;
+			dbus_out17 = 8'hzz ;
+			dbus_out18 = 8'hzz ;
+			dbus_out19 = 8'hzz ;
+			dbus_out20 = 8'hzz ;
+			dbus_out21 = 8'hzz ;
+			dbus_out22 = 8'hzz ;
+			dbus_out23 = 8'hzz ;
+			dbus_out24 = 8'hzz ;
+			dbus_out25 = 8'hzz ;
+			dbus_out26 = 8'hzz ;
+			dbus_out27 = 8'hzz ;
+			dbus_out28 = 8'hzz ;
+			dbus_out29 = 8'hzz ;
+			dbus_out30 = 8'hzz ;
+			dbus_out31 = 8'hzz ;
+			dbus_out32 = 8'hzz ;
+			dbus_out33 = 8'hzz ;
+			dbus_out34 = 8'hzz ;
+			dbus_out35 = 8'hzz ;
+			dbus_out36 = 8'hzz ;
+			dbus_out37 = 8'hzz ;
+			dbus_out38 = 8'hzz ;
+			dbus_out39 = 8'hzz ;
+			dbus_out40 = 8'hzz ;
+			dbus_out41 = 8'hzz ;
+			dbus_out42 = 8'hzz ;
+			dbus_out43 = 8'hzz ;
+			dbus_out44 = 8'hzz ;
+			dbus_out45 = 8'hzz ;
+			dbus_out46 = 8'hzz ;
+			dbus_out47 = 8'hzz ;
+			dbus_out48 = 8'hzz ;
+			dbus_out49 = 8'hzz ;
+			dbus_out50 = 8'hzz ;
 		end
     end
 endmodule
--- FPGA_model-architecture/nnctrl.v
+++ FPGA_model-architecture/nnctrl.v
@@ -1,17 +1,23 @@
 `define WIDTH 32
 
-module nnctrl ( reset, clk ) ;
+module nnctrl50 ( reset, clk, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15, in16, in17, in18, in19,  in20, in21, in22, in23, in24, in25, in26, in27, in28, in29, in30, in31, in32, in33, in34, in35, in36, in37, in38, in39, in40, in41, in42, in43, in44, in45, in46, in47, in48, in49, in50 ) ;
 	
 	input reset, clk ;
+	input[7:0] in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15, in16, in17, in18, in19,  in20, in21, in22, in23, in24, in25, in26, in27, in28, in29, in30, in31, in32, in33, in34, in35, in36, in37, in38, in39, in40, in41, in42, in43, in44, in45, in46, in47, in48, in49, in50 ;
 	reg layer, fin ;
 	reg [9:0] n, i, base ;
-	reg enRam, r_w, enRom, zero ;
-	wire [7:0] ram_dout,  rom_dout, output_data ;
-	reg [7:0] abus, addr ;
+	reg enRam, r_w, enRom, zero, insign, isbias, startsign ;
+	wire [7:0] ram_dout1, ram_dout2, ram_dout3, ram_dout4, ram_dout5, ram_dout6, ram_dout7, ram_dout8, ram_dout9, ram_dout10, ram_dout11, ram_dout12, ram_dout13, ram_dout14, ram_dout15, ram_dout16, ram_dout17, ram_dout18, ram_dout19,  ram_dout20, ram_dout21, ram_dout22, ram_dout23, ram_dout24, ram_dout25, ram_dout26, ram_dout27, ram_dout28, ram_dout29, ram_dout30, ram_dout31, ram_dout32, ram_dout33, ram_dout34, ram_dout35, ram_dout36, ram_dout37, ram_dout38, ram_dout39, ram_dout40, ram_dout41, ram_dout42, ram_dout43, ram_dout44, ram_dout45, ram_dout46, ram_dout47, ram_dout48, ram_dout49, ram_dout50 ;
+	wire [7:0] rom_dout1, rom_dout2, rom_dout3, rom_dout4, rom_dout5, rom_dout6, rom_dout7, rom_dout8, rom_dout9, rom_dout10, rom_dout11, rom_dout12, rom_dout13, rom_dout14, rom_dout15, rom_dout16, rom_dout17, rom_dout18, rom_dout19,  rom_dout20, rom_dout21, rom_dout22, rom_dout23, rom_dout24, rom_dout25, rom_dout26, rom_dout27, rom_dout28, rom_dout29, rom_dout30, rom_dout31, rom_dout32, rom_dout33, rom_dout34, rom_dout35, rom_dout36, rom_dout37, rom_dout38, rom_dout39, rom_dout40, rom_dout41, rom_dout42, rom_dout43, rom_dout44, rom_dout45, rom_dout46, rom_dout47, rom_dout48, rom_dout49, rom_dout50 ;
+	wire [7:0] output_data ;
+	reg [15:0] abus ;
+	reg [31:0] addr ;
 	
-	memory ram ( reset, enRam, r_w, abus, output_data, ram_dout ) ;
-	ROM rom ( reset, addr, rom_dout, enRom ) ;
-	neural n1 ( reset, zero, clk, output_data, ram_dout, rom_dout, output_data ) ;
+	memory50 ram ( reset, enRam, r_w, abus, insign, output_data, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15, in16, in17, in18, in19,  in20, in21, in22, in23, in24, in25, in26, in27, in28, in29, in30, in31, in32, in33, in34, in35, in36, in37, in38, in39, in40, in41, in42, in43, in44, in45, in46, in47, in48, in49, in50, ram_dout1, ram_dout2, ram_dout3, ram_dout4, ram_dout5, ram_dout6, ram_dout7, ram_dout8, ram_dout9, ram_dout10, ram_dout11, ram_dout12, ram_dout13, ram_dout14, ram_dout15, ram_dout16, ram_dout17, ram_dout18, ram_dout19,  ram_dout20, ram_dout21, ram_dout22, ram_dout23, ram_dout24, ram_dout25, ram_dout26, ram_dout27, ram_dout28, ram_dout29, ram_dout30, ram_dout31, ram_dout32, ram_dout33, ram_dout34, ram_dout35, ram_dout36, ram_dout37, ram_dout38, ram_dout39, ram_dout40, ram_dout41, ram_dout42, ram_dout43, ram_dout44, ram_dout45, ram_dout46, ram_dout47, ram_dout48, ram_dout49, ram_dout50 ) ;
+	
+	ROM50 rom ( reset, addr, rom_dout1, rom_dout2, rom_dout3, rom_dout4, rom_dout5, rom_dout6, rom_dout7, rom_dout8, rom_dout9, rom_dout10, rom_dout11, rom_dout12, rom_dout13, rom_dout14, rom_dout15, rom_dout16, rom_dout17, rom_dout18, rom_dout19,  rom_dout20, rom_dout21, rom_dout22, rom_dout23, rom_dout24, rom_dout25, rom_dout26, rom_dout27, rom_dout28, rom_dout29, rom_dout30, rom_dout31, rom_dout32, rom_dout33, rom_dout34, rom_dout35, rom_dout36, rom_dout37, rom_dout38, rom_dout39, rom_dout40, rom_dout41, rom_dout42, rom_dout43, rom_dout44, rom_dout45, rom_dout46, rom_dout47, rom_dout48, rom_dout49, rom_dout50, enRom ) ;
+	
+	neural50 n1 ( reset, zero, clk, output_data, isbias, ram_dout1, ram_dout2, ram_dout3, ram_dout4, ram_dout5, ram_dout6, ram_dout7, ram_dout8, ram_dout9, ram_dout10, ram_dout11, ram_dout12, ram_dout13, ram_dout14, ram_dout15, ram_dout16, ram_dout17, ram_dout18, ram_dout19,  ram_dout20, ram_dout21, ram_dout22, ram_dout23, ram_dout24, ram_dout25, ram_dout26, ram_dout27, ram_dout28, ram_dout29, ram_dout30, ram_dout31, ram_dout32, ram_dout33, ram_dout34, ram_dout35, ram_dout36, ram_dout37, ram_dout38, ram_dout39, ram_dout40, ram_dout41, ram_dout42, ram_dout43, ram_dout44, ram_dout45, ram_dout46, ram_dout47, ram_dout48, ram_dout49, ram_dout50, rom_dout1, rom_dout2, rom_dout3, rom_dout4, rom_dout5, rom_dout6, rom_dout7, rom_dout8, rom_dout9, rom_dout10, rom_dout11, rom_dout12, rom_dout13, rom_dout14, rom_dout15, rom_dout16, rom_dout17, rom_dout18, rom_dout19,  rom_dout20, rom_dout21, rom_dout22, rom_dout23, rom_dout24, rom_dout25, rom_dout26, rom_dout27, rom_dout28, rom_dout29, rom_dout30, rom_dout31, rom_dout32, rom_dout33, rom_dout34, rom_dout35, rom_dout36, rom_dout37, rom_dout38, rom_dout39, rom_dout40, rom_dout41, rom_dout42, rom_dout43, rom_dout44, rom_dout45, rom_dout46, rom_dout47, rom_dout48, rom_dout49, rom_dout50, output_data ) ;
 	
 	always@( posedge clk ) begin
 		// excute next
@@ -26,67 +32,103 @@ module nnctrl ( reset, clk ) ;
 			i = 0 ;
 			abus = 0 ;
 			addr = 0 ;
+			insign = 0 ;
+			isbias = 0 ;
+			startsign = 0 ;
 			// enRam = 1 ;
 			// r_w = 1 ;
 			// enRom = 1 ;
 		end
 		// get data
+		else if ( startsign == 0 ) begin
+			// store input data to ram 50 data per clock
+			// 1000 input needs 20 clocks
+			r_w = 0 ;	// write
+			enRam = 1 ;
+			abus = 50*i ;
+			i = i+1 ;
+			if ( i == 22 ) begin
+				startsign = 1 ;
+				abus = 0 ;
+				i = 0 ;
+				enRam = 0 ;
+				r_w = 1 ;
+			end
+		end
 		else if ( layer == 0 ) begin
-			if ( n == 3 ) begin
+			if ( n == 100 ) begin
+				// next layer
 				base = addr+1 ;
 				layer = 1 ;
 				n = 0 ;
 				i = 0 ;
+				insign = 0 ;
 				enRam = 0 ;
 				r_w = 1 ;
 				enRom = 0 ;
+				isbias = 0 ;
 			end
-			else if ( i == 6 ) begin
+			else if ( i == 21 ) begin
+				// write back ram
 				enRam = 1 ;
 				r_w = 0 ;
 				enRom = 0 ;
-				abus = n+6 ;
+				abus = n+1001 ;
 				n = n+1 ;
 				i = 0 ;
+				insign = 1 ;
+				isbias = 0 ;
 			end
 			else begin
 				if ( i == 0 )
 					zero = 1 ;
 				else
 					zero = 0 ;
-				abus = i ;		// 1001
-				addr = i+n*6 ;
+				abus = i*50 ;
+				addr = i*50+n*1001 ;
 				i = i+1 ;
 				enRam = 1 ;
 				r_w = 1 ;
 				enRom = 1 ;
+				insign = 0 ;
+				isbias = 0 ;
+				if ( i == 21 )
+					isbias = 1 ;
 			end
 		end
 		else if ( layer == 1 ) begin
 			if ( n == 2 ) begin
 				n = 0 ;
 				i = 0 ;
+				insign = 0 ;
+				isbias = 0 ;
 				$stop ;
 			end
-			else if ( i == 4 ) begin
+			else if ( i == 3 ) begin
 				enRam = 1 ;
 				r_w = 0 ;
 				enRom = 0 ;
 				abus = n ;
 				n = n+1 ;
 				i = 0 ;
+				insign = 1 ;
+				isbias = 0 ;
 			end
 			else begin
 				if ( i == 0 )
 					zero = 1 ;
 				else
 					zero = 0 ;
-				abus = i+6 ;		// 1001
-				addr = i+n*4+18 ;
+				abus = i*50+1001 ;
+				addr = i*50+n*101+100100 ;
 				i = i+1 ;
 				enRam = 1 ;
 				r_w = 1 ;
 				enRom = 1 ;
+				insign = 0 ;
+				isbias = 0 ;
+				if ( i == 3 )
+					isbias = 1 ;
 			end
 		end
 	end
