2d0d033af04ad015b23014b6061f4a551a06776c
SYangChen <yang870313@gmail.com>
2020-09-09 12:49:30 +0800

    4th edition architecture

---
+++ FPGA_model-architecture/classifier.v
@@ -0,0 +1,21 @@
+`define WIDTH 32
+
+module classfier ( rst, enable, data1, data2, category ) ;
+
+	input rst, enable ;
+	input [7:0] data1, data2 ;
+	output reg category ;
+
+	always@( enable or posedge rst ) begin
+		if ( rst )
+			category = 1'bz ;
+		else
+		begin
+			if ( data1 > data2 )
+				category = 1'b0 ;
+			else
+				category = 1'b1 ;
+		end
+	end
+
+endmodule
--- FPGA_model-architecture/testbench.v
+++ FPGA_model-architecture/testbench.v
@@ -3,8 +3,9 @@
 module nnctrl_tb ;
 	reg clock, reset ;
 	reg [7:0] in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15, in16, in17, in18, in19,  in20, in21, in22, in23, in24, in25, in26, in27, in28, in29, in30, in31, in32, in33, in34, in35, in36, in37, in38, in39, in40, in41, in42, in43, in44, in45, in46, in47, in48, in49, in50, count, tmp ;
-
-	nnctrl t ( reset, clock, count, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15, in16, in17, in18, in19,  in20, in21, in22, in23, in24, in25, in26, in27, in28, in29, in30, in31, in32, in33, in34, in35, in36, in37, in38, in39, in40, in41, in42, in43, in44, in45, in46, in47, in48, in49, in50 ) ;
+	wire category ;
+	
+	nnctrl t ( reset, clock, count, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15, in16, in17, in18, in19,  in20, in21, in22, in23, in24, in25, in26, in27, in28, in29, in30, in31, in32, in33, in34, in35, in36, in37, in38, in39, in40, in41, in42, in43, in44, in45, in46, in47, in48, in49, in50, category ) ;
 
 	initial // reset、clock
 	begin
@@ -206,9 +207,10 @@ module nnctrl_tb ;
 		#4600 count = 0 ;
 		#200 count = 1 ;
 		#200 count = 2 ;
-		#600 count = 0 ;
+		#800 count = 0 ;
 		#200 count = 1 ;
 		#200 count = 2 ;
+		#800 $stop ;
 	end
 	always #100 clock = clock + 1; 
 
